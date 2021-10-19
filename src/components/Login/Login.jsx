1768423d11f35752e717df4b23ee5b341b7a6818
kiran <krmate634@gmail.com>
2021-10-20 01:45:31 +0530

    api key changed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,9 @@ import { accessUrl } from "../../api/auth";
 
 export const Login = params => {
   return (
-    <div style={{ marginTop: "500px" }}>
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "40vh" }}
+    >
       <a href={accessUrl}>LOGIN TO SPOTIFY</a>
     </div>
   );
